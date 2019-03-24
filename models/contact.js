0052702189bc3d76a136af3afb3bc431b188c1c8
[leonard.achanya] <[lennardachanya.la@gmail.com]>
2019-03-24 15:14:08 +0100

    renamed column name to full_name

--- models/contact.js
+++ models/contact.js
@@ -1,28 +1,28 @@
 const db = require("../utils/database");
 
 class Contact {
-    constructor(name, phone,email) {
-        this.name = name;
+    constructor(full_name, phone,email) {
+        this.full_name = full_name;
         this.phone = phone;
         this.email = email;
     }
 
     save() {
         return db.execute(
-            "INSERT INTO contact (name, phone,email) VALUES (?, ?,?)",
-            [this.name, this.phone, this.email]);
+            "INSERT INTO contact (full_name , phone,email) VALUES (?, ?,?)",
+            [this.full_name, this.phone, this.email]);
     }
 
     static getAll() {
-        return db.execute("SELECT * FROM contact ORDER BY name");
+        return db.execute("SELECT * FROM contact ORDER BY full_name");
     }
 
     static getById(id) {
         return db.execute("SELECT * FROM contact WHERE id=?", [id])
     }
 
-    static UpdateById(name,phone,email,id) {
-        return db.execute("UPDATE contact SET name = ?, phone = ?, email = ? WHERE id=?",[name,phone,email,id])
+    static UpdateById(full_name,phone,email,id) {
+        return db.execute("UPDATE contact SET full_name = ?, phone = ?, email = ? WHERE id=?",[full_name,phone,email,id])
     }
 
     static DeleteById(id) {
